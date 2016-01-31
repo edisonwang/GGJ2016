ab9ef29cdbdfb34044066b90500a017dba5388b1
Nick Cooper <envy3d@gmail.com>
2016-01-31 14:10:45 +1300

    Merged game projects

---
+++ Assets/nick_rain_test/Scripts/AIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class AIManager : MonoBehaviour {
+
+    public RAIN.Entities.Entity guardEntity;
+    public AIRig aiRig;
+    public bool aiTurn;
+
+    void Start()
+    {
+        //guardEntity.EntityInit();
+        //aiRig.AIStart();
+        // guardEntity.
+        aiRig.AI.WorkingMemory.GetItem<bool>("aiTurn");
+    }
+
+    void Update()
+    {
+        //aiRig.AIUpdate();
+        aiRig.AI.WorkingMemory.SetItem<bool>("aiTurn", aiTurn);
+    }
+}
