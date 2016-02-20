74f37598d1479c4c538948db157df85130101ea3
Allan <allan.exire@gmail.com>
2016-02-21 01:02:24 +1300

    triggers added

---
+++ Assets/Scripts/LookDirectionTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookDirectionTrigger : MonoBehaviour
+{
+    public Vector2 directionToLook;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ForceLookDirection(other.gameObject);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        ForceLookDirection(other.gameObject);
+    }
+
+    void ForceLookDirection(GameObject go)
+    {
+        if (go.tag == "Guard")
+        {
+            go.GetComponent<Animator>().SetInteger("horizontal", (int)directionToLook.x);
+            go.GetComponent<Animator>().SetInteger("vertical", (int)directionToLook.y);
+        }
+    }
+}
